5ba3201c9f1d465cd34d8cc48ab4aa3384b9662b
Mediocre <76650975+mediocre9@users.noreply.github.com>
2022-10-10 09:19:09 +0500

    refactor: change theme param `marker` to `highlight`.

--- includes/AbstractTypes.h
+++ includes/AbstractTypes.h
@@ -1,7 +1,7 @@
 #ifndef ABSTRACT_TYPES_H
 #define ABSTRACT_TYPES_H
 
-// General Dimension class for widgets/console window...
+// General Dimension class for widgets/console window....
 struct Dimension {
     Dimension() {}
 
