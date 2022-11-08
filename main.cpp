9f38d18c5e57c77b6f7e6cbb5b79107e44ee8a8a
Fahad Zia <76650975+mediocre9@users.noreply.github.com>
2022-11-08 08:25:43 +0500

    refactor: remove color param from `Frame` class

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include "includes\Menu.h"
 #include <memory>
 
-const int DEFAULT = eng::Color.WHITE_BLACK;
-const int HIGHLIGHT = eng::Color.BRIGHT_RED_BRIGHT_WHITE;
+const int DEFAULT   = eng::Color.BRIGHT_WHITE_BLACK;
+const int HIGHLIGHT = eng::Color.BRIGHT_BLUE_BRIGHT_WHITE;
 
 void tearDownMenu();
+void subTearDownMenu();
 
 int main(){
+    
     eng::CursorState(10,false);
-    system("title Menus (demo) & mode 40,10 & cls & color 87");
+    system("title Menus (demo) & mode 60,27 & cls & color 87");
     
     
     // window frame....
-    std::unique_ptr<Window> frame(new Frame(Dimension(6,27), 
-        Coordinate(5,2), 
-        eng::Color.WHITE_BLACK)
+    std::unique_ptr<Window> frame(new Frame(Dimension(20,48), 
+        Coordinate(5,4))
     );
-    frame->setColor(0x001E);
+    frame->setBorderType(Window::PIPE);
     frame->setShadow(true);
     frame->render();
     
@@ -24,61 +25,69 @@ int main(){
             {"  File   ", HIGHLIGHT},
             {"  Edit   ", DEFAULT},
             {"  View   ", DEFAULT},
+            {"  Tools  ", DEFAULT},
+            {"  Search ", DEFAULT},
             {"  Help   ", DEFAULT},
         }, 
         Coordinate(-10, 0)
     ));
     
+    
     h_menu->setTheme(Theme(DEFAULT, HIGHLIGHT));
 
-    while(true){
+
+    for(;;) {
         
-        while(!h_menu->isItemSelected()){
+        while(h_menu->isItemNotSelected()){
             h_menu->render();
         }
         
         h_menu->restart();
         
-        switch(h_menu->getItemPosition()){
+        switch(h_menu->getItemPosition()) {
             case 1:
                 tearDownMenu();
             break;
             
             case 2:
-            	 /* Do stuff from here */
+            	/* Do stuff from here */
             break;
             
             case 3:
-            	 /* Do stuff from here */
+            	/* Do stuff from here */
             break;
             
             case 4:
-            	 /* Do stuff from here */
+            	/* Do stuff from here */
             break;
         }
         
     }
+    
+    
     return 0;
 }
 
 
 // vertical menus........
-void tearDownMenu(){
+void tearDownMenu() {
     std::unique_ptr<Menu> v_menu(new VerticalMenu({
-            {"  New    ", HIGHLIGHT},
-            {"  Open   ", DEFAULT},
-            {"  Save   ", DEFAULT},
-            {"  Back   ", DEFAULT},
+            {"  New      ", HIGHLIGHT},
+            {"  Open     ", DEFAULT},
+            {"  Save     ", DEFAULT},
+            {"  Print    ", DEFAULT},
+            {"  Export > ", DEFAULT},
+            {"  Close    ", DEFAULT},
         }, 
         Coordinate(0, 1)
     ));
     
-    v_menu->setTheme(Theme(0x0070, HIGHLIGHT));
+    v_menu->setTheme(Theme(DEFAULT, HIGHLIGHT));
 
     
     while(true){
         
-        while(!v_menu->isItemSelected()){
+        while(v_menu->isItemNotSelected()){
             v_menu->render();
         }
         
@@ -90,14 +99,60 @@ void tearDownMenu(){
             break;
             
             case 2:
-            	 /* Do stuff from here */
+            	/* Do stuff from here */
             break;
             
             case 3:
-            	 /* Do stuff from here */
+            	/* Do stuff from here */
             break;
             
-            case 4:
+            case 5:
+            	subTearDownMenu();
+            break;
+            
+            case 6:
+            	main();
+            break;
+        }
+    }
+}
+
+
+void subTearDownMenu() {
+    std::unique_ptr<Menu> v_menu(new VerticalMenu({
+            {"  Date   ", HIGHLIGHT},
+            {"  Time   ", DEFAULT},
+            {"  Insert ", DEFAULT},
+            {"  Header ", DEFAULT},
+            {"  Close  ", DEFAULT},
+        }, 
+        Coordinate(11, 5)
+    ));
+    
+    v_menu->setTheme(Theme(DEFAULT, HIGHLIGHT));
+    
+    while(true){
+        
+        while(v_menu->isItemNotSelected()){
+            v_menu->render();
+        }
+        
+        v_menu->restart();
+        
+        switch(v_menu->getItemPosition()){
+            case 1:
+                /* Do stuff from here */
+            break;
+            
+            case 2:
+            	/* Do stuff from here */
+            break;
+            
+            case 3:
+            	/* Do stuff from here */
+            break;
+            
+            case 5:
             	main();
             break;
         }
